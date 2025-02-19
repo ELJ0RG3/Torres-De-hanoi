466bb3b9afa81cd6de03896c5c5d5ac86b9d4656
jorge <jorge@DESKTOP-3CMP1JT>
2025-02-19 17:05:13 -0600

    Agregar archivos de proyecto.

---
+++ Torres De hanoi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Implementa el juego de Torres de Hanoi utilizando pilas para representar cada torre.
+//El programa debe permitir:
+//Ingresar el número de discos con los que se jugará.
+//Mostrar el estado de las torres en cada paso.
+
+namespace Torres_De_hanoi
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
