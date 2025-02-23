09b140a07c44c1e541f3e413cb40256dde8d0087
ELJ0RG3 <Jorge.ocana@edu.uag.mx>
2025-02-23 17:03:17 -0600

    Update Program.cs

--- Torres De hanoi/Program.cs
+++ Torres De hanoi/Program.cs
@@ -1,20 +1,57 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-//Implementa el juego de Torres de Hanoi utilizando pilas para representar cada torre.
-//El programa debe permitir:
-//Ingresar el número de discos con los que se jugará.
-//Mostrar el estado de las torres en cada paso.
 
-namespace Torres_De_hanoi
+namespace Torres_De_Hanoi
 {
     internal class Program
     {
+        static void MostrarEstado(List<int> torre1, List<int> torre2, List<int> torre3)
+        {
+            Console.WriteLine("Estado actual de las torres:");
+            Console.WriteLine("Torre 1: " + string.Join(", ", torre1));
+            Console.WriteLine("Torre 2: " + string.Join(", ", torre2));
+            Console.WriteLine("Torre 3: " + string.Join(", ", torre3));
+            Console.WriteLine();
+        }
+
+        static void ResolverTorres(int n, List<int> origen, List<int> destino, List<int> auxiliar)
+        {
+            if (n == 1)
+            {
+                destino.Add(origen[origen.Count - 1]);
+                origen.RemoveAt(origen.Count - 1);
+                MostrarEstado(origen, destino, auxiliar);
+                return;
+            }
+
+            ResolverTorres(n - 1, origen, auxiliar, destino);
+
+            destino.Add(origen[origen.Count - 1]);
+            origen.RemoveAt(origen.Count - 1);
+            MostrarEstado(origen, destino, auxiliar);
+
+            ResolverTorres(n - 1, auxiliar, destino, origen);
+        }
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Ingrese el número de discos:");
+            int discos = int.Parse(Console.ReadLine());
+
+            List<int> torre1 = new List<int>();
+            List<int> torre2 = new List<int>();
+            List<int> torre3 = new List<int>();
+
+            for (int i = discos; i > 0; i--)
+            {
+                torre1.Add(i);
+            }
+
+            MostrarEstado(torre1, torre2, torre3);
+
+            ResolverTorres(discos, torre1, torre3, torre2);
+
+            Console.WriteLine("El juego ha terminado.");
         }
     }
 }
